5d9e1a4ddc3a1bedde4e284658d707cbe23645ac
Landon Fuller <landonf@bikemonkey.org>
2013-09-13 22:38:19 -0400

    Sketch out a main window view.

---
+++ Antenna/ANTMainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ANTMainWindowController.h
+//  Antenna
+//
+//  Created by Landon Fuller on 9/13/13.
+//  Copyright (c) 2013 Plausible Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANTMainWindowController : NSWindowController
+
+@end
---
+++ Antenna/ANTMainWindowController.m
@@ -0,0 +1,33 @@
+//
+//  ANTMainWindowController.m
+//  Antenna
+//
+//  Created by Landon Fuller on 9/13/13.
+//  Copyright (c) 2013 Plausible Labs. All rights reserved.
+//
+
+#import "ANTMainWindowController.h"
+
+@interface ANTMainWindowController ()
+
+@end
+
+@implementation ANTMainWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
