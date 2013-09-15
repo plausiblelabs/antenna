e030ec42341226d05ae76ccfabcdc19b69be0a0b
Landon Fuller <landonf@bikemonkey.org>
2013-09-14 21:55:55 -0400

    Checkpoint basic working source list.

--- Antenna/ANTRadarsWindowController.m
+++ Antenna/ANTRadarsWindowController.m
@@ -27,47 +27,190 @@
  */
 
 #import "ANTRadarsWindowController.h"
+#import <PLFoundation/PLFoundation.h>
 
-@interface ANTRadarsWindowController ()
+@interface ANTRadarsWindowController () @end
+
+@interface ANTRadarsWindowSourceItem : NSObject <NSOutlineViewDataSource, NSOutlineViewDelegate, NSCopying>
+
++ (instancetype) itemWithTitle: (NSString *) title
+                      children: (NSArray *) children;
+
++ (instancetype) itemWithTitle: (NSString *) title
+                          icon: (NSImage *) icon;
+
+- (instancetype) initWithTitle: (NSString *) title icon: (NSImage *) icon children: (NSArray *) children;
+
+/** The source item's title. */
+@property(nonatomic, readonly) NSString *title;
+
+/** The item's icon, if any. */
+@property(nonatomic, readonly) NSImage *icon;
+
+/** Child elements, if any. */
+@property(nonatomic, readonly) NSArray *children;
 
 @end
 
 /**
  * Manages the primary 'viewer' window.
  */
-@implementation ANTRadarsWindowController
+@implementation ANTRadarsWindowController {
+@private
+    /** Left-side source tree */
+    __weak IBOutlet NSOutlineView *_outlineView;
+
+    /** Items listed in the left-hand source view */
+    NSArray *_sourceItems;
+}
 
 - (id) init {
     if ((self = [super initWithWindowNibName: [self className] owner: self]) == nil)
         return nil;
     
+    ANTRadarsWindowSourceItem *radarItem = [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"My Radars", nil) children: @[
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Open", nil) icon: nil],
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Closed", nil) icon: nil],
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Recently Updated", nil) icon: [NSImage imageNamed: NSImageNameFolderSmart]]
+    ]];
+
+    ANTRadarsWindowSourceItem *openRadarItem = [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"My Public Radars", nil) children: @[
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Open", nil) icon: nil],
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Closed", nil) icon: nil],
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Watching", nil) icon: [NSImage imageNamed: NSImageNameFolderSmart]],
+        [ANTRadarsWindowSourceItem itemWithTitle: NSLocalizedString(@"Recently Updated", nil) icon: [NSImage imageNamed: NSImageNameFolderSmart]]
+    ]];
+
+    _sourceItems = @[radarItem, openRadarItem];
+
     return self;
 }
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+
+    /* Expand all by default. TODO: Should we save/restore the user's preferences here? */
+    [_outlineView expandItem: nil expandChildren: YES];
 }
 
+- (NSView *) outlineView: (NSOutlineView *) outlineView viewForTableColumn: (NSTableColumn *) column item: (id) treeNode {
+    ANTRadarsWindowSourceItem *item = treeNode;
+    NSTableCellView *view;
+
+    if ([_sourceItems containsObject: item]) {
+        view = [outlineView makeViewWithIdentifier: @"HeaderCell" owner: self];
+    } else {
+        view = [outlineView makeViewWithIdentifier: @"DataCell" owner: self];
+        if (item.icon != nil) {
+            [[view imageView] setImage: item.icon];
+        } else {
+            [[view imageView] setImage: [NSImage imageNamed: NSImageNameFolder]];
+        }
+    }
+    
+    [[view textField] setStringValue: item.title];
+    return view;
+}
+
+// from NSOutlineViewDataSource protocol
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item {
+    /* Top-level items are not selectable */
+    if ([_sourceItems containsObject: item])
+        return NO;
+    
+    return YES;
+}
+
+// from NSOutlineViewDataSource protocol
+- (BOOL) outlineView: (NSOutlineView *) outlineView isGroupItem: (id) item {
+    /* Top-level items use the group style */
+    if ([_sourceItems containsObject: item])
+        return YES;
+
+    return NO;
+}
+
+// from NSOutlineViewDataSource protocol
 - (NSInteger) outlineView: (NSOutlineView *) outlineView numberOfChildrenOfItem: (id) item {
     if (item == nil)
-        return 2;
-    return 0;
+        return [_sourceItems count];
+
+    return ((ANTRadarsWindowSourceItem *) item).children.count;
 }
 
+// from NSOutlineViewDataSource protocol
 - (id) outlineView: (NSOutlineView *) outlineView child: (NSInteger) index ofItem: (id) item {
     if (item == nil)
-        return @"Item";
-    return nil;
+        return _sourceItems[index];
+
+    return ((ANTRadarsWindowSourceItem *) item).children[index];
 }
 
+// from NSOutlineViewDataSource protocol
 - (id) outlineView: (NSOutlineView *) outlineView objectValueForTableColumn: (NSTableColumn *) tableColumn byItem: (id) item {
     return item;
 }
 
+// from NSOutlineViewDataSource protocol
 - (BOOL) outlineView: (NSOutlineView *) outlineView isItemExpandable: (id) item {
-    return YES;
+    if (((ANTRadarsWindowSourceItem *) item).children.count > 0)
+        return YES;
+
+    return NO;
+}
+
+@end
+
+/**
+ * A single source window item.
+ */
+@implementation ANTRadarsWindowSourceItem
+
+/**
+ * Return a new instance.
+ *
+ * @param title The instance's title.
+ * @param children Child elements.
+ */
++ (instancetype) itemWithTitle: (NSString *) title children: (NSArray *) children {
+    return [[self alloc] initWithTitle: title icon: nil children: children];
+}
+
+/**
+ * Return a new instance.
+ *
+ * @param title The instance's title.
+ * @param icon The instance's custom icon, or nil to use the default.
+ */
++ (instancetype) itemWithTitle: (NSString *) title icon: (NSImage *) icon {
+    return [[self alloc] initWithTitle: title icon: icon children: nil];
+}
+
+
+/**
+ * Initialize a new instance.
+ *
+ * @param title Title of the source item.
+ * @param children Child elements.
+ */
+- (instancetype) initWithTitle: (NSString *) title icon: (NSImage *) icon children: (NSArray *) children {
+    PLSuperInit();
+    
+    _title = title;
+    _children = children;
+    _icon = icon;
+
+    return self;
+}
+
+// from NSCopying protocol
+- (instancetype) copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+// from NSCopying protocol
+- (instancetype) copy {
+    return [self copyWithZone: nil];
 }
 
 @end
